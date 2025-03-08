6a9d51eecec21f8486bc2dd77f692953f31e5a7e
nimitaya <sonja.schweren@web.de>
2025-03-08 14:45:33 +0100

    add desktop navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,37 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Navigation = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // refeerenz zum Mobile Menü
+  const menuRef = useRef(null);
 
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);      
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // bei Klick irgendwo soll zugemacht werden
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  // EventListener auf dem document, wenn man klickt
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, []);
 
   return (
     <nav>
       {/* Mobile Navigation */}
-      <div className="mobile-dropdown">
-        <div tabIndex={0} role="button" className="mobile-btn" onClick={toggleMenu} onKeyDown={toggleMenu}>
+      <div className="mobile-dropdown" ref={menuRef}>
+        <div
+          tabIndex={0}
+          role="button"
+          className="mobile-btn"
+          onClick={toggleMenu}
+          onKeyDown={toggleMenu}
+        >
           {/* SVG for Button */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width={50}
-            height={50}
-            fill="none"
+            id="Layer_1"
+            data-name="Layer 1"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            width="40"
+            height="40"
           >
-            {" "}
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />{" "}
+            <path d="M0,3.5c0-.83,.67-1.5,1.5-1.5H17.5c.83,0,1.5,.67,1.5,1.5s-.67,1.5-1.5,1.5H1.5c-.83,0-1.5-.67-1.5-1.5Zm17.5,14.5H1.5c-.83,0-1.5,.67-1.5,1.5s.67,1.5,1.5,1.5H17.5c.83,0,1.5-.67,1.5-1.5s-.67-1.5-1.5-1.5Zm5-8H6.5c-.83,0-1.5,.67-1.5,1.5s.67,1.5,1.5,1.5H22.5c.83,0,1.5-.67,1.5-1.5s-.67-1.5-1.5-1.5Z" fill="#461e5c" />
           </svg>
         </div>
         {/* <div className="helper-div"></div> */}
-        <ul tabIndex={0} className={`mobile-menu ${isMenuOpen ? 'open' : ''}`}>
+        <ul tabIndex={0} className={`mobile-menu ${isMenuOpen ? "open" : ""}`}>
           <li>
             <a href="#">Home</a>
           </li>
@@ -57,13 +74,13 @@ const Navigation = () => {
             <a href="#">Home</a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a href="#about">About</a>
           </li>
           <li>
-            <a href="#">Projects</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
